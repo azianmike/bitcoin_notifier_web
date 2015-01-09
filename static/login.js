f27fcb7266719e3911a322fde5dbf5df5bc90396
Ubuntu <ubuntu@ip-172-31-59-144.ec2.internal>
2015-01-09 21:57:46 +0000

    Got login working with seperate JS file

--- static/login.js
+++ static/login.js
@@ -1,9 +1,22 @@
 function login(){
+    console.log("enter login");
         var paramsJSON = {};
-        paramsJSON['email'] = document.getElementById("email").value;
-        paramsJSON['password'] = hashPassword(document.getElementById("password").value);
-        postToUrl("/registerJSON/", paramsJSON);
-    } 
- 
+        paramsJSON['email'] = document.getElementById("loginEmail").value;
+        paramsJSON['password'] = hashPassword(document.getElementById("loginPassword").value);
+        postToUrl("/loginJSON/", paramsJSON, loginCallback);
 }
 
+function loginCallback(jsonData){
+        if(jsonData["success"] == "1"){
+          // this means the user has been registere
+          alert("Success!");
+          window.location.replace("/");
+          } else if(jsonData["success"] == "0"){
+            alert("Error: Email does not exist or password is wrong");
+          } else {
+            alert("Fatal error");
+        }
+
+
+
+}
