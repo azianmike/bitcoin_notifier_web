26fd208c0320a9ea1ea7d9ae66bb3baffdf3f1a0
michaelluo <michaeluo@gmail.com>
2015-02-18 22:06:22 -0600

    Fixed small syntax error in register.js

--- static/register.js
+++ static/register.js
@@ -16,6 +16,7 @@ function register(){
 
 function validateEmail(email) { 
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    console.log(re.test(email))
     return re.test(email);
 } 
 
